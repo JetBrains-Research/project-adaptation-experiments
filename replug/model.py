806f253815fa8e5003ebd2b154dc3a811dbb2d28
Mikhail Arkhipov <mikhail.arkhipov@jetbrains.com>
2024-07-25 18:46:49 +0000

    style: explicit weight in test

--- replug/model.py
+++ replug/model.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
                         'Remember Chad ', 
                         'Remember Cuba ']
     file_prefix = '\n\nSo I know the following five countries: '
-    examples = [RePlugExample(cv + file_prefix, None, None, None, None) for cv in context_variants]
+    examples = [RePlugExample(cv + file_prefix, None, None, None, None, 1.0) for cv in context_variants]
     device = 'cuda:2'
     model_inputs = RePlugInstance(examples)
     model = RePlugModel('gpt2', device)
