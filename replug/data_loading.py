e4ab76551e7ed1326a50b84638cf8e1ecb15dd07
Mikhail Arkhipov <mikhail.arkhipov@jetbrains.com>
2024-07-25 14:41:38 +0000

    add: replug instance (collection of context + comp)

--- replug/data_loading.py
+++ replug/data_loading.py
@@ -1,3 +1,4 @@
+import os
 import random
 from dataclasses import dataclass
 
@@ -11,14 +12,14 @@ class RepoStorage:
 
 
 @dataclass
-class CompletionFile:
+class FileStorage:
     filename: str
     content: str
 
 
 @dataclass
 class RawDatapoint:
-    completion_file: CompletionFile
+    completion_file: FileStorage
     repo_snapshot: RepoStorage
     completion_lines: dict[str, list[int]]
 
@@ -35,6 +36,9 @@ class RePlugExample:
     prefix: str
     splitted_file: SplittedFile
     line_category: str
+    context_file: FileStorage
+    completion_file: FileStorage
+    context_weight: float = 1.0
 
     @property
     def ground_truth(self):
@@ -47,6 +51,47 @@ class RePlugExample:
     @property
     def file_postfix(self):
         return self.splitted_file.postfix
+
+
+def path_distance(base_path, target_path):
+    base_components = os.path.normpath(base_path).split(os.sep)
+    target_components = os.path.normpath(target_path).split(os.sep)
+
+    common_length = len(os.path.commonprefix([base_components, target_components]))
+
+    distance = (len(base_components) - common_length) + (len(target_components) - common_length)
+    return distance - 1
+
+
+@dataclass
+class RePlugInstance:
+    examples: list[RePlugExample]
+
+    def __len__(self):
+        return len(self.examples)
+    
+    def __iter__(self):
+        return iter(self.examples)
+    
+    def define_context_weights(self, context_weights: list[float] | None = None):
+        if context_weights is None:
+            context_weights = [1.0 / len(self)] * len(self)
+        for example, weight in zip(self.examples, context_weights):
+            example.context_weight = weight
+
+    def get_top_k_contexts(self, k: int) -> "RePlugInstance":
+        return RePlugInstance(sorted(self.examples, key=lambda x: x.context_weight, reverse=True)[:k])
+
+    def calculate_path_distances_weights(self):
+        for example in self.examples:
+            pd = path_distance(example.context_file.filename, example.completion_file.filename)
+            example.context_weight = 1 / pd
+
+    def normalize_context_weights(self):
+        weight_sum = sum([example.context_weight for example in self.examples])
+        for example in self.examples:
+            example.context_weight /= weight_sum
+
 
 
 PATTERN = '# {fn_repo}\n\n{cont_repo}\n\n# {fn_compl}\n\n{file_prefix}'
@@ -75,7 +120,7 @@ def sample_repo_snapshot(full_repo: RepoStorage, sample_size: int) -> RepoStorag
 
 
 def get_raw_datapoint(ds: Dataset, idx: int, sample_size: int = 100) -> RawDatapoint:
-    completion_file = CompletionFile(**ds[idx]['completion_file'])
+    completion_file = FileStorage(**ds[idx]['completion_file'])
     repo_snapshot = RepoStorage(**ds[idx]['repo_snapshot'])
     sampled_repo_snapshot = sample_repo_snapshot(repo_snapshot, sample_size)
     return RawDatapoint(
@@ -111,22 +156,32 @@ def get_examples_from_raw_datapoint(raw_datapoint: RawDatapoint) -> list[list[Re
                                     file_prefix=splitted_file.prefix),
                         splitted_file=splitted_file,
                         line_category=line_cat,
+                        context_file=FileStorage(filename, content),
+                        completion_file=FileStorage(raw_datapoint.completion_file.filename, splitted_file.prefix)
                     )
                 )
-            example_batches.append(example_contexts)
+            example_batches.append(RePlugInstance(example_contexts))
+        
     return example_batches
 
 
 if __name__ == '__main__':
     ds = load_dataset('JetBrains-Research/lca-project-level-code-completion', config_name, split='test')
-    raw_dp = get_raw_datapoint(ds, 0, sample_size=3)
+    raw_dp = get_raw_datapoint(ds, 0, sample_size=100)
     example_batches = get_examples_from_raw_datapoint(raw_dp)
     # print(examples)
     # print(raw_dp)
     for example_batch in example_batches:
+        example_batch.calculate_path_distances_weights()
+        example_batch = example_batch.get_top_k_contexts(3)
+        print(example_batch.examples[0].completion_file.filename)
         for example in example_batch:
-            print('='*100)
-            print(example.prefix[:1_000] + '\n\n...\n\n', example.prefix[-1_000:])
-            print('-'*100)
-            print(example.ground_truth)
+            print(example.context_weight)
+            print(example.context_file.filename)
         break
+        # for example in example_batch:
+        #     print('='*100)
+        #     print(example.prefix[:1_000] + '\n\n...\n\n', example.prefix[-1_000:])
+        #     print('-'*100)
+        #     print(example.ground_truth)
+        # break
