5c753a650fcdd6db6162bd16b56b4272cf67c487
Mikhail Arkhipov <mikhail.arkhipov@jetbrains.com>
2024-07-25 19:18:18 +0000

    upd: inference for new model

--- replug/inference.py
+++ replug/inference.py
@@ -1,55 +1,35 @@
 from datasets import load_dataset
-from data_loading import get_raw_datapoint, get_examples_from_raw_datapoint
-from model import RePlugModel
-from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+from tqdm import tqdm
+
+from data_loading import get_examples_from_raw_datapoint, get_all_raw_data_points
+from model import RePlugModel
 
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 model_name = 'deepseek-ai/deepseek-coder-1.3b-base'
-base_model = AutoModelForCausalLM.from_pretrained(model_name,
-                                                  torch_dtype=torch.bfloat16,
-                                                  attn_implementation='flash_attention_2').to(device)
-model = RePlugModel(base_model)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = RePlugModel(model_name, device)
 
 ds = load_dataset('JetBrains-Research/lca-project-level-code-completion', 'medium_context', split='test')
-raw_dp = get_raw_datapoint(ds, 54, sample_size=25)
-all_examples = get_examples_from_raw_datapoint(raw_dp)
-for examples in all_examples:
-    print(examples[0]['line_cat'])
-    if examples[0]['line_cat'] == 'inproject':
-        print('Found inproject example')
-        break
-else:
-    print('No inproject example found')
-    raise
 
+all_raw_data_points = get_all_raw_data_points(ds, extension_to_get='.py')
 
-batch = []
-for example in examples:
-    input_ids = tokenizer(example['prefix'], return_tensors='pt', truncation=True, max_length=16000)['input_ids'].to(device)
-    print(input_ids.shape)
-    batch.append(input_ids)
-gt = example['ground_truth']
+instances = []
 
-with torch.inference_mode():
-    outputs = model.generate(batch, max_new_tokens=25)[0][:, -25:]
+for raw_dp in tqdm(all_raw_data_points, desc='Processing raw data points'):
+    curr_instances = get_examples_from_raw_datapoint(raw_dp, line_cat_to_get='inproject')
+    instances.extend(curr_instances)
 
-file_prefix_input_ids = tokenizer(example['file_prefix'], return_tensors='pt')['input_ids'].to(device)
+print(f'Got {len(instances)} instances')
 
-only_prefix_ouput = base_model.generate(file_prefix_input_ids, max_new_tokens=25)[:, -25:]
-only_prefix_text = tokenizer.batch_decode(only_prefix_ouput, skip_special_tokens=True)
-
-generated = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-print('Line cat: ', example['line_cat'])
-print('Only prefix: ')
-print(only_prefix_text[0])
-
-for g in generated:
+for instance in instances:
     print('=' * 100)
-    print(g)
-print('-' * 100)
-print(gt)
+    print('Line cat: ', instance.line_category)
+    generated = model.generate(instance, max_new_tokens=25)
+    print('---- Generated ----\n', generated)
+    print('--- Ground truth ----\n', instance.ground_truth)
+    print('-' * 100)
+
+
 
 
