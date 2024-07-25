156e90f8dbaa3e0afa2ad06e5dc3d04f7ea2d968
Mikhail Arkhipov <mikhail.arkhipov@jetbrains.com>
2024-07-25 17:19:31 +0000

    feat: kv cache

--- replug/model.py
+++ replug/model.py
@@ -3,26 +3,45 @@ from torch import nn
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from tqdm import tqdm
 
+from data_loading import RePlugInstance, RePlugExample
+
 
 class RePlugModel(nn.Module):
-    def __init__(self, base_model):
+    def __init__(self, model_name: str, device: str | torch.DeviceObjType):
         super(RePlugModel, self).__init__()
-        self.base_model = base_model
+        self.base_model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                               torch_dtype=torch.bfloat16,
+                                                               attn_implementation='flash_attention_2',
+                                                               device_map=device)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.device = torch.device(device)
 
-    def generate_one_token(self, list_input_ids: list[torch.LongTensor]):
-        out_list = list()
-        for input_ids in list_input_ids:
-            out = self.base_model(input_ids)
-            out_list.append(out.logits[:, -1:, ...])
-        aggr_out = self._aggregate_outs(out_list)
-        new_token = torch.argmax(aggr_out, dim=-1, keepdim=True)
-        new_list_input_ids = [torch.cat([input_ids, new_token], dim=-1) for input_ids in list_input_ids]
-        return new_list_input_ids
+    def generate(self, input_instance: RePlugInstance, max_new_tokens: int = 10) -> str:
+        past_kvs = [None] * len(input_instance)
+        current_input_ids = []
 
-    def generate(self, list_input_ids: list[torch.LongTensor], max_new_tokens: int = 10):
+        for sample in input_instance:
+            inputs = self.tokenizer(sample.prefix, return_tensors='pt')
+            input_ids = inputs['input_ids'].to(self.device)
+            current_input_ids.append(input_ids)
+
+        generated = []
         for _ in tqdm(range(max_new_tokens)):
-            list_input_ids = self.generate_one_token(list_input_ids)
-        return list_input_ids
+            logits_list = []
+            new_kvs = []
+            for i, (input_ids, cur_kv) in enumerate(zip(current_input_ids, past_kvs)):
+                out = self.base_model(input_ids, past_key_values=cur_kv)
+                logits = out.logits[:, -1:]
+                logits_list.append(logits)
+                new_kvs.append(out.past_key_values)
+            aggr_out = self._aggregate_outs(logits_list)
+            new_token = torch.argmax(aggr_out, dim=-1, keepdim=True)
+            generated.append(new_token)
+            current_input_ids = [new_token] * len(input_instance)
+            past_kvs = new_kvs
+        generated = torch.cat(generated, dim=1)
+        generated_text = self.tokenizer.decode(generated[0])
+        return generated_text
 
     def _aggregate_outs(self, out_list):
         # out_list = []
@@ -33,14 +52,15 @@ class RePlugModel(nn.Module):
 
 
 if __name__ == '__main__':
-    base_model = AutoModelForCausalLM.from_pretrained("gpt2")
-    tokenizer = AutoTokenizer.from_pretrained("gpt2")
-    context_variants = ['Remember Cambodia ', 'Remember Mongolia ', 'Remember Vanuatu ', 'Remember Chad ', 'Remember Cuba ']
+    context_variants = ['Remember Cambodia ',
+                        'Remember Mongolia ', 
+                        'Remember Vanuatu ',
+                        'Remember Chad ', 
+                        'Remember Cuba ']
     file_prefix = '\n\nSo I know the following five countries: '
-    examples = [cv + file_prefix for cv in context_variants]
-    list_input_ids = [tokenizer(example, return_tensors='pt')['input_ids'] for example in examples]
-    model = RePlugModel(base_model)
-    output = model.generate(list_input_ids, max_new_tokens=25)
+    examples = [RePlugExample(cv + file_prefix, None, None, None, None) for cv in context_variants]
+    device = 'cuda:2'
+    model_inputs = RePlugInstance(examples)
+    model = RePlugModel('gpt2', device)
+    output = model.generate(model_inputs, max_new_tokens=25)
     print(output)
-    for _output in output:
-        print(tokenizer.batch_decode(_output))
