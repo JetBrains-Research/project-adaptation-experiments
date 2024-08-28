926c8a2d8dc6c8d8202e7c519e1a78dfcba4de8b
evgeniy glukhov <evgeniy.glukhov@jetbrains.com>
2024-08-28 12:57:30 +0000

    Implement data processing and KL scorer

---
+++ rag_experiments/data_loading.py
@@ -0,0 +1,154 @@
+from dataclasses import dataclass
+from typing import Iterator
+
+from datasets import load_dataset
+
+
+@dataclass
+class FileStorage:
+    filename: str
+    content: str
+
+@dataclass
+class RepoStorage:
+    filename: list[str]
+    content: list[str]
+
+    def __post_init__(self):
+        if not len(self.filename) == len(self.content):
+            raise ValueError('Filenames and Content must have the same lengths')
+
+    def __len__(self):
+        return len(self.filename)
+
+    def __iter__(self) -> Iterator[FileStorage]:
+        for filename, content in zip(self.filename, self.content):
+            yield FileStorage(filename, content)
+
+
+@dataclass
+class Chunk:
+    filename: str
+    chunk_idx: int
+    max_chunks: int
+    content: str
+    score: float | None = None
+
+    def __str__(self):
+        num_lines = len(self.content.split("\n"))
+        return (f'Chunk {self.chunk_idx} out of {self.max_chunks} of file {self.filename}, '
+                f'Len: {len(self.content)}C/{num_lines}L, Score: {self.score:.4f}')
+
+
+@dataclass
+class ChunkedFile:
+    filename: str
+    content_chunks: list[str]
+
+    def __len__(self):
+        return len(self.content_chunks)
+
+    def flatten(self) -> list[Chunk]:
+        result = list()
+        for chunk_idx, chunk in enumerate(self.content_chunks):
+            result.append(
+                Chunk(filename=self.filename, chunk_idx=chunk_idx, max_chunks=self.__len__(), content=chunk)
+            )
+        return result
+
+
+@dataclass
+class ChunkedRepo:
+    chunks: list[Chunk] | None = None
+
+    def __post_init__(self):
+        if self.chunks is None:
+            self.chunks = list()
+
+    def append(self, chunked_file: ChunkedFile):
+        if not isinstance(chunked_file, ChunkedFile):
+            raise ValueError('You can append only ChunkedFile')
+        self.chunks.extend(chunked_file.flatten())
+
+    def __iter__(self) -> Iterator[Chunk]:
+        for chunk in self.chunks:
+            yield chunk
+
+    def __len__(self):
+        return len(self.chunks)
+
+    def set_scores(self, scores: list[float]):
+        if len(scores) != len(self.chunks):
+            raise ValueError('Scores must correspond to chunks')
+        for score, chunk in zip(scores, self.chunks):
+            chunk.score = score
+
+
+def get_file_and_repo(dp) -> tuple[FileStorage, RepoStorage]:
+    hf_repo_filename = dp.pop('repo_snapshot_filename', None)
+    if hf_repo_filename is None:
+        repo_snapshot = RepoStorage(**dp['repo_snapshot'])
+    else:
+        # TODO: get filenames map file and finish loading the dataset
+        repo_snapshot = None
+    completion_file = FileStorage(**dp['completion_file'])
+    return completion_file, repo_snapshot
+
+
+def chunk_py_file_content(file_st: FileStorage, chunk_lines_size: int = 32, overlap_lines_size: int = 8) -> ChunkedFile:
+    if chunk_lines_size <= overlap_lines_size:
+        raise ValueError('chunk_lines_size must be greater than overlap_lines_size')
+    lines = file_st.content.split('\n')
+    # TODO: filter out lines with whitespace chars (spaces, tabs)
+    chunks = list()
+    while len(lines) > chunk_lines_size:
+        chunks.append('\n'.join(lines[:chunk_lines_size]))
+        lines = lines[(chunk_lines_size - overlap_lines_size):]
+    chunks.append('\n'.join(file_st.content.split('\n')[-chunk_lines_size:]))
+    return ChunkedFile(file_st.filename, chunks)
+
+
+def chunk_repository(repo_snapshot: RepoStorage, **chunking_kwargs):
+    chunked_repo = ChunkedRepo()
+    for file_st in repo_snapshot:
+        if len(file_st.content.strip()) > 1:
+            if file_st.filename.endswith('.py'):
+                chunked_file = chunk_py_file_content(file_st, **chunking_kwargs)
+                chunked_repo.append(chunked_file)
+                # chunked_repo[file_st.filename] = chunks
+    return chunked_repo
+
+
+def score_chunks(completion_file: FileStorage, chunked_repo: ChunkedRepo) -> None:
+    for chunk in chunked_repo:
+        chunk.score = len(chunk.content) / len(completion_file.content) + len(chunk.filename) / len(completion_file.filename)
+
+def main():
+    ds = load_dataset('JetBrains-Research/lca-project-level-code-completion', 'medium_context', split='test')
+    completion_file, repo_snapshot = get_file_and_repo(ds[0])
+    chunk_kwargs = {'chunk_lines_size': 64, 'overlap_lines_size': 8}
+    chunked_repo = chunk_repository(repo_snapshot, **chunk_kwargs)
+    for idx, chunk in enumerate(chunked_repo):
+        if idx > 10:
+            print('-' * 100)
+            break
+        print(chunk)
+    score_chunks(completion_file, chunked_repo)
+    for idx, chunk in enumerate(chunked_repo):
+        if idx > 10:
+            print('-' * 100)
+            break
+        print(chunk)
+    # return ds['train'][0]
+
+
+if __name__ == '__main__':
+    main()
+    # dp = main()
+    # print(dp.keys())
+    # print(dp['repo_snapshot_filename'])
+    # repo_snapshot = load_dataset('jenyag/plcc-python-train',
+    #                              data_files=['repo_data/*/unicef__iogt__33f09c0453f2c6f08060000217aeb814420102c9.parquet'],
+    #                              split='train')
+    # ds = load_dataset('JetBrains-Research/lca-project-level-code-completion', 'medium_context', split='test')
+    # file, repo = get_file_and_repo(ds[0])
---
+++ rag_experiments/kl_rag.py
@@ -0,0 +1,91 @@
+import torch
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from rag_experiments.data_loading import FileStorage, ChunkedRepo, get_file_and_repo, chunk_repository
+
+
+class KLScorer:
+    def __init__(self, model_name: str, device: str | torch.DeviceObjType):
+        self.base_model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                               torch_dtype=torch.bfloat16,
+                                                               attn_implementation='flash_attention_2',
+                                                               device_map=device)
+        self.base_model.eval()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.tokenizer.truncation_side = 'left'
+        self.device = torch.device(device)
+
+    @torch.inference_mode()
+    def get_file_level_logits(self, completion_ids) -> torch.Tensor:
+        completion_logits = self.base_model(completion_ids).logits
+        return completion_logits
+
+    @torch.inference_mode()
+    def get_chunk_logits(self, chunk_ids, completion_ids) -> torch.Tensor:
+        input_ids = torch.cat([chunk_ids, completion_ids[:, 1:]], dim=-1)
+        chunk_logits = self.base_model(input_ids).logits
+        return chunk_logits
+
+    @torch.inference_mode()
+    def _convert_to_logprobs(self, logits_tensor: torch.Tensor) -> torch.Tensor:
+        return torch.log_softmax(logits_tensor, dim=-1)
+
+    @torch.inference_mode()
+    def compare_logprobs(self, chunk_logprobs: torch.Tensor, file_level_logprobs: torch.Tensor) -> torch.Tensor:
+        kl_scores = torch.nn.functional.kl_div(
+            chunk_logprobs[0, -int(file_level_logprobs.shape[1]):, :],
+            file_level_logprobs[0, -int(file_level_logprobs.shape[1]):, :],
+            reduction='none',
+            log_target=True
+        ).mean(dim=-1)
+        return kl_scores
+
+    def get_token_ids(self, text: str) -> torch.Tensor:
+        text_tokenized = self.tokenizer(text, return_tensors='pt')
+        token_ids = text_tokenized['input_ids'].to(self.device)
+        return token_ids
+
+    @torch.inference_mode()
+    def score_repo(self, completion_file: FileStorage, chunked_repo: ChunkedRepo,
+                   completion_file_truncate_lines: int = -1) -> list[float]:
+        scores = list()
+        if completion_file_truncate_lines < 1:
+            completion_ids = self.get_token_ids(completion_file.content)
+        else:
+            completion_lines = completion_file.content.split('\n')
+            truncated_completion = '\n'.join(completion_lines[:completion_file_truncate_lines])
+            completion_ids = self.get_token_ids(truncated_completion)
+        file_level_logits = self.get_file_level_logits(completion_ids)
+        file_level_logprobs = self._convert_to_logprobs(file_level_logits)
+        for chunk in chunked_repo:
+            chunk_ids = self.get_token_ids(chunk.content)
+            chunk_logits = self.get_chunk_logits(chunk_ids, completion_ids)
+            chunk_logprobs = self._convert_to_logprobs(chunk_logits)
+            kl_scores = self.compare_logprobs(chunk_logprobs, file_level_logprobs)
+            scores.append(kl_scores.mean().item()*1e6)
+        return scores
+
+
+def main():
+    ds = load_dataset('JetBrains-Research/lca-project-level-code-completion', 'medium_context', split='test')
+    completion_file, repo_snapshot = get_file_and_repo(ds[0])
+    chunk_kwargs = {'chunk_lines_size': 100, 'overlap_lines_size': 8}
+    chunked_repo = chunk_repository(repo_snapshot, **chunk_kwargs)
+    device_num = 1
+    device = f'cuda:{device_num}' if torch.cuda.is_available() else 'cpu'
+    scorer = KLScorer(
+        model_name='deepseek-ai/deepseek-coder-1.3b-base',
+        device=device
+    )
+    scores = scorer.score_repo(completion_file, chunked_repo, completion_file_truncate_lines=100)
+    chunked_repo.set_scores(scores)
+    for idx, chunk in enumerate(chunked_repo):
+        if idx > 250:
+            print('-' * 100)
+            break
+        print(chunk)
+
+
+if __name__ == '__main__':
+    main()
