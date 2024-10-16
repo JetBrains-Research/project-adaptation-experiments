0b43d1ac04460d9a19d0c5a95dc3aa4e1e1bfc1c
galtimur <gal_timur@yahoo.com>
2024-10-16 16:23:22 +0200

    Merged configs

--- rag_experiments/eval_on_rag.py
+++ rag_experiments/eval_on_rag.py
@@ -50,89 +50,89 @@ def ammend_summary(config_eval, config_rag):
         writer.write_all(records)
 
 
-def run_eval_plcc(eval_config_path: str, rag_config_path: str, limit: int = -1) -> None:
+def run_eval_plcc(config_path: str = "config_plcc.yaml", limit: int = -1) -> None:
     '''
         eval_config_path: str
             Path to yaml config
         limit: int
             Number of batches in dataloader
     '''
-    config_eval = OmegaConf.load(eval_config_path)
-    results_filename = Path(config_eval.output.results_filename)
-    config_eval.output.results_filename = results_filename
-    config_rag = OmegaConf.load(rag_config_path)
+    config = OmegaConf.load(config_path)
+    results_filename = Path(config.output.results_filename)
+    config.output.results_filename = results_filename
+    config_rag = config.rag
 
     generation_engine = VllmEngine(
-        hf_model_path=config_eval.model.model_name_or_path,
-        model_name=config_eval.model.get("model_name"),
-        context_size=config_eval.eval.context_size,
-        vllm_args=dict(config_eval.vllm.vllm_args),
-        generation_args=dict(config_eval.vllm.generation_args),
+        hf_model_path=config.model.model_name_or_path,
+        model_name=config.model.get("model_name"),
+        context_size=config.eval.context_size,
+        vllm_args=dict(config.vllm.vllm_args),
+        generation_args=dict(config.vllm.generation_args),
     )
     evaluator = Evaluator(
         engine=generation_engine,
-        result_folder=config_eval.output.result_folder,
-        result_filename=config_eval.output.results_filename,
+        result_folder=config.output.result_folder,
+        result_filename=config.output.results_filename,
         # log_model_inputs = config_eval.eval.log_model_inputs,
-        config=config_eval
+        config=config
     )
 
     print(40 * "-")
-    print(f"Composer - {config_eval.data.composer_name}")
-    print(f"Model - {config_eval.model.model_name_or_path}")
-    if config_eval.data.composer_name == "kl_chunk_score":
+    print(f"Composer - {config.data.composer_name}")
+    print(f"Model - {config.model.model_name_or_path}")
+    if config.data.composer_name == "kl_chunk_score":
         device_num = 1
         device = f"cuda:{device_num}" if torch.cuda.is_available() else "cpu"
         scorer = KLScorer(model_name=config_rag.model, device=device)
         context_composer = ChunkScoreComposer(
-            language=config_eval.data.language,
-            rag_config=config_rag,
+            language=config.data.language,
+            rag_config=config.rag,
             scorer=scorer,
         )
-    elif config_eval.data.composer_name == "iou_chunk_score":
+    elif config.data.composer_name == "iou_chunk_score":
         # scorer = IOUChunkScorer(model_name=config_rag.model)
         splitter = ModelSplitter(model_name=config_rag.model)
         scorer = IOUScorer(splitter)
         chunker = FixedLineChunker()
         context_composer = ChunkScoreComposer(
-            language=config_eval.data.language,
+            language=config.data.language,
             chunker=chunker,
             rag_config=config_rag,
             scorer=scorer,
         )
-    elif config_eval.data.composer_name == "iou_file_score":
+    elif config.data.composer_name == "iou_file_score":
         context_composer = FileScoreComposer(
-            language=config_eval.data.language,
+            language=config.data.language,
             top_k=config_rag.top_k,
             iou_type=config_rag.iou_file_type,
-            model_name=config_eval.model.model_name_or_path,
+            model_name=config.model.model_name_or_path,
         )
-    elif config_eval.data.composer_name == "from_file":
+    elif config.data.composer_name == "from_file":
         context_composer = FromFileComposer(
-            language=config_eval.data.language,
-            dataset_path=config_eval.data.composer_dataset_file,
+            language=config.data.language,
+            dataset_path=config.data.composer_dataset_file,
         )
-    elif config_eval.data.composer_name == "no_context":
+    elif config.data.composer_name == "no_context":
         context_composer = BaseContextComposer(
-            language=config_eval.data.language,
-            allowed_extensions=config_eval.data.allowed_extensions,
+            language=config.data.language,
+            allowed_extensions=config.data.allowed_extensions,
         )
-    elif config_eval.data.composer_name == "path_distance":
+    elif config.data.composer_name == "path_distance":
         context_composer = PathDistanceComposer(
-            filter_extensions=config_eval.data.filter_extensions,
-            language=config_eval.data.language,
-            allowed_extensions=config_eval.data.allowed_extensions,
-            completion_categories=config_eval.data.completion_categories,
-            topk=config_eval.data.topk,
+            filter_extensions=config.data.filter_extensions,
+            language=config.data.language,
+            allowed_extensions=config.data.allowed_extensions,
+            completion_categories=config.data.completion_categories,
+            topk=config.data.topk,
         )
     else:
-        raise ValueError(f"There is no {config_eval.data.composer_name} composer")
+        raise ValueError(f"There is no {config.data.composer_name} composer")
 
-    for ctx_len in config_eval.eval.context_size_list:
+    for ctx_len in config.eval.context_size_list:
         print(f"Context length = {ctx_len}")
-        config_eval.eval.context_size = ctx_len
+        config.eval.context_size = ctx_len
 
-        dataloader = get_dataloader(config_eval, context_composer)
+        dataloader = get_dataloader(config, context_composer)
 
         summary = evaluator.eval(dataloader, limit=limit)
         # TODO fix output filename
