b96d364a163f2a594a1e3435a1d5280c590e1f1f
galtimur <gal_timur@yahoo.com>
2024-10-18 17:03:26 +0200

    Fixed dataset

--- rag/bug_localization/load_data.py
+++ rag/bug_localization/load_data.py
@@ -6,18 +6,14 @@ def load_data(languages: list[str]) -> list[dict]:
 
     all_data = []
     for lang in languages:
-
         data_file = hf_hub_download(
             repo_id="galtimur/lca-bug-localization-test",
             filename=f"dataset/{lang}.jsonl",
             repo_type="dataset",
+            force_download=True
         )
-
         with jsonlines.open(data_file) as reader:
             data = list(reader)
-
-        for item in data:
-            item["language"] = lang
         all_data.extend(data)
 
     return all_data
