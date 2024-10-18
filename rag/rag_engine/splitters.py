39d6ba6107f0264bdf3e311e491187c06d7bf6f1
galtimur <gal_timur@yahoo.com>
2024-10-18 18:26:58 +0200

    Added word-splitter

--- rag/rag_engine/splitters.py
+++ rag/rag_engine/splitters.py
@@ -1,8 +1,8 @@
 from transformers import AutoTokenizer
-
+import re
 
 class BaseSplitter:
-    def __init__(self, model_name: str | None = None):
+    def __init__(self, **kwargs):
         pass
 
     def __call__(self, string: str) -> list[str | int]:
@@ -10,12 +10,18 @@ class BaseSplitter:
 
 
 class ModelSplitter(BaseSplitter):
-    def __init__(self, model_name: str):
+    def __init__(self, model_name: str, **kwargs):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.tokenizer.truncation_side = "left"
 
     def __call__(self, string: str) -> list[int]:
         return self.tokenizer.encode(string)
+
+class WordSplitter(BaseSplitter):
+
+    def __call__(self, string: str) -> list[str]:
+        tokens = re.findall(r"[\w]+", string)
+        return tokens
 
 
 class LinesSplitter(BaseSplitter):
@@ -33,9 +39,13 @@ def get_splitter(name: str, **kwargs) -> BaseSplitter:
         splitter = ModelSplitter(**kwargs)
     elif name == "line_splitter":
         splitter = LinesSplitter(**kwargs)
+    elif name == "word_splitter":
+        splitter = WordSplitter(**kwargs)
     else:
         raise ValueError(
-            f"There is no {name} splitter. Only [model_tokenizer, line_splitter] are available"
+            f"There is no {name} splitter. Only [model_tokenizer, line_splitter, word_splitter] are available"
         )
+
+
 
     return splitter
